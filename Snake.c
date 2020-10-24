bf0e94e3e6776178a8be5bf2fbda0e11033f09d3
Noel Ho <s.ho@digipen.edu>
2020-10-25 03:25:30 +0800

    Improved food checking and added colour

--- Snake.c
+++ Snake.c
@@ -1,25 +1,30 @@
 #include "Snake.h"
 #include <stdio.h>
 
+float TILE_SIZE = 30.f;
+int GRID_START_X = 30;
+int GRID_START_Y = 150;
+
 int grid[GRID_WIDTH][GRID_HEIGHT] = { 0 };
-//CP_Vector snake[GRID_WIDTH * GRID_HEIGHT] = { 0 };
-//int snake_size = 0;
-//int snake_direction = Right;
-//float snake_speed_multiplier = 4.0f;
 float snake_speed_timer = 0.0f;
 
 int food_exists = 0;
-//int to_grow = 0;
-//int SNAKE_SPEED = 1;
 int game_over = 0;
 char* text = "";
 struct Snake_Profile snake1;
 
-void Snake_Init()
+void Level_Init()
 {
 	CP_System_SetWindowSize(900, 900);
+	// Adjust tile size according to level size
+	TILE_SIZE = (((float)CP_System_GetWindowHeight() - (float)GRID_START_Y - 5) / (float)GRID_HEIGHT);
+	// Centers the level
+	GRID_START_X = (int)((float)CP_System_GetWindowWidth() - (GRID_WIDTH * TILE_SIZE)) / 2;
+}
+
+void Snake_Init()
+{
 	// add snake of size 1 at (0,0)
-	//Snake_AddSnake(0, 0);
 	for (int i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++)
 	{
 		snake1.Position[i].x = 0;
@@ -31,7 +36,9 @@ void Snake_Init()
 	snake1.Speed = 1;
 	snake1.to_grow = 0;
 	snake1.is_alive = 1;
-	Snake_AddSnake(0, 0, &snake1);
+	snake1.HeadColor = GREEN;
+	snake1.BodyColor = DARK_GREEN;
+	Snake_GrowSnake(0, 0, &snake1);
 }
 
 void Snake_Update(const float dt)
@@ -40,7 +47,7 @@ void Snake_Update(const float dt)
 	Snake_UpdateSnake(dt);
 	if (CP_Input_MouseClicked()) {
 		Snake_SpawnFood();
-		Snake_GrowSnake();
+		snake1.to_grow = 1;
 	}
 	if (!food_exists) {
 		Snake_SpawnFood();
@@ -52,12 +59,12 @@ void Snake_Render()
 {
 	// render the grid x and y
 	for (int x = 0; x < GRID_WIDTH+1; x++) {
-		int x0 = GRID_START_X + x * TILE_SIZE;
-		CP_Graphics_DrawLine((float)x0, (float)GRID_START_Y, (float)x0, (float)GRID_START_Y + GRID_HEIGHT * TILE_SIZE);
+		float x0 = GRID_START_X + x * TILE_SIZE;
+		CP_Graphics_DrawLine(x0, (float)GRID_START_Y, x0, (float)GRID_START_Y + GRID_HEIGHT * TILE_SIZE);
 	}
 	for (int y = 0; y < GRID_WIDTH+1; y++) {
-		int y0 = GRID_START_X + y * TILE_SIZE;
-		CP_Graphics_DrawLine((float)GRID_START_X, (float)y0, (float)GRID_START_X + GRID_WIDTH * TILE_SIZE, (float)y0);
+		float y0 = GRID_START_Y + y * TILE_SIZE;
+		CP_Graphics_DrawLine((float)GRID_START_X, y0, (float)GRID_START_X + GRID_WIDTH * TILE_SIZE, y0);
 	}
 	// render snake
 	Snake_DrawSnake();
@@ -66,6 +73,7 @@ void Snake_Render()
 	for (int y = 0; y < GRID_HEIGHT; y++) {
 		for (int x = 0; x < GRID_WIDTH; x++) {
 			if (grid[y][x] == 2) {
+				CP_Settings_Fill(GREY);
 				CP_Graphics_DrawCircle((float)x * TILE_SIZE + (TILE_SIZE/2) + GRID_START_X, (float)y * TILE_SIZE + (TILE_SIZE/2) + GRID_START_Y, TILE_SIZE);
 			}
 		}
@@ -85,23 +93,26 @@ void Snake_DrawSnake()
 {
 	// for each cell in snake, draw a square there of tile size
 	for (int i = 0; i < snake1.Size; i++) {
-		CP_Graphics_DrawRect(snake1.Position[i].x * TILE_SIZE + GRID_START_X, snake1.Position[i].y * TILE_SIZE + GRID_START_Y, TILE_SIZE, TILE_SIZE);
+		if (i == 0)
+		{ CP_Settings_Fill(snake1.HeadColor); }
+		else { CP_Settings_Fill(snake1.BodyColor); }
+		CP_Graphics_DrawRect(snake1.Position[i].x * TILE_SIZE + GRID_START_X, snake1.Position[i].y * TILE_SIZE + GRID_START_Y, TILE_SIZE, TILE_SIZE);		
 	}
 }
 
 void Snake_UpdateSnake(const float dt)
 {
 	// SNAKE INPUT
-	if (CP_Input_KeyDown(KEY_RIGHT) && snake1.Direction != Left) {
+	if (CP_Input_KeyDown(KEY_RIGHT) && snake1.PreviousDirection != Left) {
 		snake1.Direction = Right;
 	}
-	if (CP_Input_KeyDown(KEY_LEFT) && snake1.Direction != Right) {
+	if (CP_Input_KeyDown(KEY_LEFT) && snake1.PreviousDirection != Right) {
 		snake1.Direction = Left;
 	}
-	if (CP_Input_KeyDown(KEY_UP) && snake1.Direction != Down) {
+	if (CP_Input_KeyDown(KEY_UP) && snake1.PreviousDirection != Down) {
 		snake1.Direction = Up;
 	}
-	if (CP_Input_KeyDown(KEY_DOWN) && snake1.Direction != Up) {
+	if (CP_Input_KeyDown(KEY_DOWN) && snake1.PreviousDirection != Up) {
 		snake1.Direction = Down;
 	}
 	// UPDATE SNAKE POSITION
@@ -131,10 +142,11 @@ void Snake_UpdateSnake(const float dt)
 			snake1.Position[0].y += 1;
 			break;
 		}
+		snake1.PreviousDirection = snake1.Direction;
 		// if snake collide with food
 		if (grid[(int)snake1.Position[0].y][(int)snake1.Position[0].x] == 2) {
-			Snake_GrowSnake();
-			food_exists = 0;
+			snake1.to_grow = 1;
+			//Check_For_Food();
 		}
 		// game over conditions - hit itself
 		if (grid[(int)snake1.Position[0].y][(int)snake1.Position[0].x] == 1) {
@@ -146,7 +158,7 @@ void Snake_UpdateSnake(const float dt)
 		}
 		// set last position of snake in grid to 0
 		if (snake1.to_grow) { // if to grow, add a new snake cell at last position
-			Snake_AddSnake((int)last_position.x, (int)last_position.y, &snake1);
+			Snake_GrowSnake((int)last_position.x, (int)last_position.y, &snake1);
 			snake1.to_grow = 0;
 		}
 		else { // else update grid to let know empty
@@ -154,10 +166,17 @@ void Snake_UpdateSnake(const float dt)
 		}
 		// set new position of head in grid
 		grid[(int)snake1.Position[0].y ][(int)snake1.Position[0].x] = 1;
+
+		Check_For_Food();
+	}
+
+	if (CP_Input_KeyDown(KEY_R))
+	{
+		Reset_Game();
 	}
 }
 
-void Snake_AddSnake(const int x, const int y, struct Snake_Profile *snake)
+void Snake_GrowSnake(const int x, const int y, struct Snake_Profile *snake)
 {
 
 	if (snake->Size < GRID_WIDTH * GRID_HEIGHT) {
@@ -169,23 +188,41 @@ void Snake_AddSnake(const int x, const int y, struct Snake_Profile *snake)
 	}
 }
 
-void Snake_GrowSnake()
-{
-	snake1.to_grow = 1;
-}
-
 void Snake_SpawnFood()
 {
 	int rand_x;
 	int rand_y;
 	int check = 1;
 	while (check) {
-		rand_x = CP_Random_RangeInt(0, GRID_WIDTH);
-		rand_y = CP_Random_RangeInt(0, GRID_HEIGHT);
+		rand_x = CP_Random_RangeInt(0, GRID_WIDTH-1);
+		rand_y = CP_Random_RangeInt(0, GRID_HEIGHT-1);
 		// if not at a snake position
 		if (grid[rand_y][rand_x] != 1) {
 			check = 0;
 		}
 	}
 	grid[rand_y][rand_x] = 2;
+}
+
+void Reset_Game()
+{
+	Level_Init();
+	Snake_Init();
+}
+
+void Check_For_Food()
+{
+	for (int i = 0; i < GRID_WIDTH; i++)
+	{
+		for (int j = 0; j < GRID_HEIGHT; j++)
+		{
+			if (grid[i][j] == 2)
+			{
+				food_exists = 1;
+				return;
+			}
+		}
+	}
+	food_exists = 0;
+	return;
 }
--- Snake.h
+++ Snake.h
@@ -3,10 +3,19 @@
 
 #define GRID_WIDTH 16	// size of the arena square
 #define GRID_HEIGHT 16
-#define TILE_SIZE 30	// size of each arena tile
 
-#define GRID_START_X 30
-#define GRID_START_Y 30
+#define RED CP_Color_Create(255,0,0,255)
+#define DARK_RED CP_Color_Create(155,0,0,255)
+#define GREEN CP_Color_Create(0,255,0,255)
+#define DARK_GREEN CP_Color_Create(0,155,0,255)
+#define BLUE CP_Color_Create(0,0,255,255)
+#define DARK_BLUE CP_Color_Create(0,0,155,255)
+#define GREY CP_Color_Create(100,100,100,255)
+
+float TILE_SIZE;	// size of each arena tile
+
+int GRID_START_X;
+int GRID_START_Y;
 
 //int SNAKE_SPEED;
 
@@ -26,14 +35,18 @@ typedef enum Snake_Direction {
 struct Snake_Profile
 {
 	int Speed;
-	float Speed_Multiplier;
 	int Size;
+	float Speed_Multiplier;
 	CP_Vector Position[GRID_WIDTH * GRID_HEIGHT];
+	Snake_Direction PreviousDirection;
 	Snake_Direction Direction;
 	char to_grow;
 	char is_alive;
+	CP_Color HeadColor;
+	CP_Color BodyColor;
 };
 
+void Level_Init();
 void Snake_Init();
 void Snake_Update(const float dt);
 void Snake_Render();
@@ -41,8 +54,9 @@ void Snake_Free();
 
 void Snake_DrawSnake();
 void Snake_UpdateSnake(const float dt);
-//void Snake_AddSnake(const int x, const int y);
-void Snake_AddSnake(const int x, const int y, struct Snake_Profile *snake);
+void Snake_GrowSnake(const int x, const int y, struct Snake_Profile *snake);
 
-void Snake_GrowSnake();
 void Snake_SpawnFood();
+
+void Reset_Game();
+void Check_For_Food();
